3a5a9f6787ae09b484e5375cf7c2f017338ef027
Scott Mcleod <scott.r.mcleod@gmail.com>
2014-04-27 23:14:13 -0700

    Cleaned up the imports yet again.

--- src/main/java/me/olddragon/seenem/SeenEm.java
+++ src/main/java/me/olddragon/seenem/SeenEm.java
@@ -11,7 +11,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SeenEm extends JavaPlugin {
